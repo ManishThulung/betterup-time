94b4de16a41452d8609575c03fd9f8b53e4de230
ManishThulung <manish.thulung58@gmail.com>
2025-06-15 14:25:16 +0545

    worker setup

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,7 +2,7 @@ import { prismaClient } from "@repo/db/prismaClient";
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json());
 
@@ -32,12 +32,57 @@ app.post("/api/user", async (req: Request, res: Response) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
-app.post("/api/website", (req: Request, res: Response) => {
-  res.status(200).send("Website endpoint hit");
+
+app.get("/api/users", async (req: Request, res: Response) => {
+  try {
+    const users = await prismaClient.user.findMany();
+
+    if (!users) {
+      res.status(404).json({ error: "Users not found" });
+    }
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-app.get("/api/website/:status", (req: Request, res: Response) => {
-  res.status(200).send(`Website status: ${req.params.status}`);
+app.post("/api/website", async (req: Request, res: Response) => {
+  const { url, title } = req.body;
+  try {
+    const website = await prismaClient.website.create({
+      data: {
+        url,
+        title,
+        userId: "cmbmmz64k0000ki55x8lk7t0x",
+      },
+    });
+    res.status(201).json(website);
+  } catch (error: any) {
+    console.error("Error creating website:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/api/website/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(400).json({ error: "Website ID is required" });
+  }
+  try {
+    const website = await prismaClient.website.findUnique({
+      where: { id },
+    });
+    if (!website) {
+      res.status(404).json({ error: "Website not found" });
+      return;
+    }
+    res.status(200).json(website);
+  } catch (error) {
+    console.error("Error fetching website:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 app.listen(PORT, () => {
